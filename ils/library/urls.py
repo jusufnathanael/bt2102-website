3e2df08c1a5dfa82e53918714c73e1e9255345eb
jusufnathanael <jusuf.n@gmail.com>
2021-03-20 00:46:06 +0800

    Done book list and searching

--- ils/library/urls.py
+++ ils/library/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("books/<int:book>id", views.details, name="details"),
+    path("borrow/<int:bookid>", views.borrow, name="borrow"),
+    path("reserve/<int:bookid>", views.reserve, name="reserve"),
+    path("reset/<int:bookid>", views.reset, name="reset"),
+    path("search", views.search, name="search")
 ]
--- ils/library/views.py
+++ ils/library/views.py
@@ -1,6 +1,56 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, reverse
+from django.db import connection
+
+from . models import Book
 
 # Create your views here.
 def index(request):
+    c = connection.cursor()
+    q = "SELECT * FROM BOOK"
+    c.execute(q)
+    results = c.fetchall()
+    return render(request, "library/index.html", {
+        "books": results
+    })
+
+def details(request):
     return HttpResponse("")
+
+def borrow(request, bookid):
+    c = connection.cursor()
+    q = f"UPDATE BOOK SET AVAILABILITY = 'BORROWED' WHERE BOOKID = {bookid}"
+    c.execute(q)
+    q = "SELECT * FROM BOOK"
+    c.execute(q)
+    results = c.fetchall()
+    return HttpResponseRedirect(reverse("index"))
+
+def reserve(request, bookid):
+    c = connection.cursor()
+    q = f"UPDATE BOOK SET AVAILABILITY = 'RESERVED' WHERE BOOKID = {bookid}"
+    c.execute(q)
+    q = "SELECT * FROM BOOK"
+    c.execute(q)
+    results = c.fetchall()
+    return HttpResponseRedirect(reverse("index"))
+
+def reset(request, bookid):
+    c = connection.cursor()
+    q = f"UPDATE BOOK SET AVAILABILITY = 'AVAILABLE' WHERE BOOKID = {bookid}"
+    c.execute(q)
+    q = "SELECT * FROM BOOK"
+    c.execute(q)
+    results = c.fetchall()
+    return HttpResponseRedirect(reverse("index"))
+
+def search(request):
+    title = request.GET["q"]
+    # if the query matches
+    c = connection.cursor()
+    q = f"SELECT * FROM BOOK WHERE TITLE REGEXP '{title}'"
+    c.execute(q)
+    results = c.fetchall()
+    return render(request, "library/index.html", {
+        "books": results
+    })
