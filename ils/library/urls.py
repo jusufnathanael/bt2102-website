939ac1188f70ab696eebb4a8f5f42928e417e0b1
jusufnathanael <jusuf.n@gmail.com>
2021-03-20 14:19:05 +0800

    Update list of books

--- ils/library/urls.py
+++ ils/library/urls.py
@@ -3,9 +3,18 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("books/<int:book>id", views.details, name="details"),
+    path("books/<int:bookid>", views.details, name="details"),
     path("borrow/<int:bookid>", views.borrow, name="borrow"),
     path("reserve/<int:bookid>", views.reserve, name="reserve"),
-    path("reset/<int:bookid>", views.reset, name="reset"),
-    path("search", views.search, name="search")
+    path("restore/<int:bookid>", views.restore, name="restore"),
+    path("search", views.search, name="search"),
+
+    # ADMIN USER
+    path("books/borrowed", views.borrowed, name="borrowed"),
+    path("books/reserved", views.reserved, name="reserved"),
+    
+    # USER AUTHENTICATION
+    # path("register", views.register, name="register"),
+    # path("login", views.login, name="login"),
+    # path("logout", views.logout, name="logout"),
 ]
--- ils/library/views.py
+++ ils/library/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, reverse
 from django.db import connection
+from datetime import date
 
 from . models import Book
 
@@ -14,16 +15,24 @@ def index(request):
         "books": results
     })
 
-def details(request):
-    return HttpResponse("")
+def details(request, bookid):
+    c = connection.cursor()
+    q = f"SELECT * FROM BOOK WHERE BOOKID = {bookid}"
+    c.execute(q)
+    result = c.fetchone()
+    return render(request, "library/book.html", {
+        "book": result
+    })
 
 def borrow(request, bookid):
     c = connection.cursor()
     q = f"UPDATE BOOK SET AVAILABILITY = 'BORROWED' WHERE BOOKID = {bookid}"
     c.execute(q)
-    q = "SELECT * FROM BOOK"
+    '''
+    value = (bookid, 'Aldo', date.today().strftime("%Y-%m-%d"), 'BORROWED', 0)
+    q = f"INSERT INTO BORROWS VALUES {value}"
     c.execute(q)
-    results = c.fetchall()
+    '''
     return HttpResponseRedirect(reverse("index"))
 
 def reserve(request, bookid):
@@ -35,7 +44,7 @@ def reserve(request, bookid):
     results = c.fetchall()
     return HttpResponseRedirect(reverse("index"))
 
-def reset(request, bookid):
+def restore(request, bookid):
     c = connection.cursor()
     q = f"UPDATE BOOK SET AVAILABILITY = 'AVAILABLE' WHERE BOOKID = {bookid}"
     c.execute(q)
@@ -54,3 +63,32 @@ def search(request):
     return render(request, "library/index.html", {
         "books": results
     })
+
+def borrowed(request):
+    c = connection.cursor()
+    q = f"SELECT * FROM BOOK WHERE AVAILABILITY = 'BORROWED'"
+    c.execute(q)
+    results = c.fetchall()
+    return render(request, "library/index.html", {
+        "books": results
+    })
+
+def reserved(request):
+    c = connection.cursor()
+    q = f"SELECT * FROM BOOK WHERE AVAILABILITY = 'RESERVED'"
+    c.execute(q)
+    results = c.fetchall()
+    return render(request, "library/index.html", {
+        "books": results
+    })
+
+'''
+def register(request):
+    return render(request, "library/login.html")
+
+def login(request):
+    return render(request, "library/login.html")
+
+def logout(request):
+    return render(request, "library/login.html")
+'''
